2a61a23687bbf7ebf0c877637d1bde78c40ac91d
joey57 <njorogejoey5@gmail.com>
2022-04-25 17:33:43 +0300

    add more categories

--- app/views.py
+++ app/views.py
@@ -12,7 +12,9 @@ def index():
     '''
     # getting popular movie
     popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     print(popular_movies)
     title = 'Home - Welcome to the best Movie Review Webdite Online'
-    return render_template('index.html', title = title,popular = popular_movies)
+    return render_template('index.html', title = title,popular = popular_movies, upcoming = upcoming_movie, now_showing= now_showing_movie)
   
