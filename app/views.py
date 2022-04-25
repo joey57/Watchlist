786739c9fddae825249c63d092260355c4719e06
joey57 <njorogejoey5@gmail.com>
2022-04-25 13:21:45 +0300

    create movie html

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
+from email import message
 from flask import render_template
 from app import app
 # views
-@app.route('/')
-def index():
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
   '''
   view root page function that return the index page and its data 
   '''
-  return render_template('index.html')
+  return render_template('movie.html', id = movie_id)
   
