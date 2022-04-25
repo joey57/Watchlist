25cde81f40ab701fafd831ad5f460d5250157aca
joey57 <njorogejoey5@gmail.com>
2022-04-25 17:26:22 +0300

    make api call to get popular category of movies

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from email import message
 from turtle import title
 from flask import render_template
 from app import app
+from .request import get_movies
 # views
 @app.route('/')
 def index():
@@ -9,6 +10,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     title = 'Home - Welcome to the best Movie Review Webdite Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular = popular_movies)
   
