ee4a348282fd142e66c6f9823c0c82151347ddc3
joey57 <njorogejoey5@gmail.com>
2022-04-25 12:16:30 +0300

    import flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
-# initializing applicatin
+
+# initializing application
 app = Flask(__name__)
 from app import views
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import imp
 from app import app 
+
 if __name__ == '__main__':
   app.run(debug=True)
