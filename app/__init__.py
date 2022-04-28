b4248d1061d9c232e8b8efde0d5ae8c08041a131
joey57 <njorogejoey5@gmail.com>
2022-04-28 12:33:56 +0300

    update init file to create application factory functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from ..config import DevConfig
+from config import config_options
 
-# initializing application
-app = Flask(__name__,instance_relative_config=True)
+bootstrap = Bootstrap()
 
-# setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+  
+  app = Flask(__name__)
 
-# initializing Flask Extensions
-bootstrap = Bootstrap(app)
+  # creating the app configurations
+  app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+  #Initializing flask extensions
+  bootstrap.init_app(app)
+
+  # will add views and forms
+
+  return app
