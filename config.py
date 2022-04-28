629d9b48750905f2d299cd32a0f58c167e2361f1
joey57 <njorogejoey5@gmail.com>
2022-04-28 12:09:23 +0300

    update config file and import os module

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
 class Config:
   '''
   General configuration parent class
   '''
   MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+  MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig:
   '''
@@ -19,4 +22,9 @@ class DevConfig(Config):
        Config: The parent configuration class with general configuration settings
   '''
   DEBUG = True
+
+config_options ={
+  'development':DevConfig,
+  'production' :ProdConfig
+}  
 
