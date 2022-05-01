e395ed26da69ec2102b8d496da36f432e7518333
joey57 <njorogejoey5@gmail.com>
2022-05-01 12:34:06 +0300

    intialize sql database

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
   MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
   MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
   SECRET_KEY = os.environ.get('SECRET_KEY')
+  SQLALCHMEY_DATABASE_URI ='postgresql+psycopg2://postgres:whalien52@localhost/watchlist'
 
 class ProdConfig:
   '''
