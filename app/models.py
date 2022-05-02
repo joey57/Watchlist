0cc8e832d6b0cffa5e0586bf42ba4cdc864d978f
joey57 <njorogejoey5@gmail.com>
2022-05-02 11:41:32 +0300

    correct indentation

--- app/models.py
+++ app/models.py
@@ -38,13 +38,10 @@ class Review:
         response.append(review)
     return response    
 
-class User:
-  '''
-  user class to create new users
-  '''
-  __tablename__ = 'users'
-  id = db.Column(db.Integer, primary_key =True)
-  username = db.Column(db.String(255))
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
 
-  def __repr__(self):
-    return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
