6b264ac4679571035a3ab146195c2f9fb13f50cf
joey57 <njorogejoey5@gmail.com>
2022-05-02 19:22:13 +0300

    create login class form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 import email
 from . import views,forms
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -19,4 +19,10 @@ class RegistrationForm(FlaskForm):
 
   def validate_username(self,data_field):
     if User.query.filter_by(username = data_field.data).first():
-      raise ValidationError('That username is taken')   
+      raise ValidationError('That username is taken') 
+
+class LoginForm(FlaskForm):
+  email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+  password = PasswordField('Password',validators=[DataRequired()])
+  remember = BooleanField('Remember me')
+  submit = SubmitField('Sign in')
