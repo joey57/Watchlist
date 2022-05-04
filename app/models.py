2ae43b6cbeaf75076d7e9975c4c35027f185ca80
joey57 <njorogejoey5@gmail.com>
2022-05-04 11:06:15 +0300

    update review class to db

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -19,31 +20,42 @@ class Movie:
     self.vote_average = vote_average
     self.vote_count = vote_count
 
-class Review:
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+
   
-  all_reviews = []
+  # all_reviews = []
 
-  def __init__(self, movie_id, title, imageurl, review):
-    self.movie_id = movie_id
-    self.title = title
-    self.imageurl = imageurl
-    self.review = review
+  # def __init__(self, movie_id, title, imageurl, review):
+  #   self.movie_id = movie_id
+  #   self.title = title
+  #   self.imageurl = imageurl
+  #   self.review = review
 
-  def save_review(self):
-    Review.all_reviews.append(self)
+  # def save_review(self):
+  #   Review.all_reviews.append(self)
 
-  @classmethod
-  def clear_reviews(cls):
-    Review.all_reviews.clear() 
+  # @classmethod
+  # def clear_reviews(cls):
+  #   Review.all_reviews.clear() 
 
-  @classmethod
-  def get_reviews(cls, id):
-    response = []
+  # @classmethod
+  # def get_reviews(cls, id):
+  #   response = []
     
-    for review in cls.all_reviews:
-      if review.movie_id == id:
-        response.append(review)
-    return response    
+  #   for review in cls.all_reviews:
+  #     if review.movie_id == id:
+  #       response.append(review)
+  #   return response    
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -55,6 +67,7 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path =db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
+    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
