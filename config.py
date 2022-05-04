504f6df4feb02d62d7d06255c932584803344d47
joey57 <njorogejoey5@gmail.com>
2022-05-04 15:09:58 +0300

    update config file to add test db

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
   MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
   MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
   SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:whalien52@localhost/watchlist'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
@@ -30,6 +29,8 @@ class ProdConfig:
     Config: The parent configuration class with General configuration settings
   '''
   pass
+class TestConfig(Config):
+   SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:whalien52@localhost/watchlist_test'
 
 class DevConfig(Config):
   '''
@@ -37,10 +38,12 @@ class DevConfig(Config):
   Args: 
        Config: The parent configuration class with general configuration settings
   '''
+  SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:whalien52@localhost/watchlist'
   DEBUG = True
 
 config_options ={
   'development':DevConfig,
-  'production' :ProdConfig
+  'production' :ProdConfig,
+  'test' :TestConfig
 }  
 
