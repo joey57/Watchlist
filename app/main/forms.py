1c408d3b3116fd81d4396bb43b94543b1e6f3296
joey57 <njorogejoey5@gmail.com>
2022-05-04 16:32:06 +0300

    remove validator in review, reviewform

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 class ReviewForm(FlaskForm):
   title = StringField('Review title',validators=[DataRequired()])
-  review = TextAreaField('Movie review', validators=[DataRequired()])
+  review = TextAreaField('Movie review')
   submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
