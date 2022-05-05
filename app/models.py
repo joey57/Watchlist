2e2729104af368ee6b6bcf31a7f33c4da155fe7e
joey57 <njorogejoey5@gmail.com>
2022-05-05 16:15:42 +0300

    update requirements txt

--- app/models.py
+++ app/models.py
@@ -61,8 +61,8 @@ class User(UserMixin,db.Model):
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-      return check_password_hash(self.password_hash,password)    
-        
+      return check_password_hash(self.password_hash,password)
+          
     def __repr__(self):
         return f'User {self.username}'
 
