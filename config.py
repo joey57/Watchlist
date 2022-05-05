21c367fb712878cb730d02acf8b79111a4f9e7c9
joey57 <njorogejoey5@gmail.com>
2022-05-05 19:59:18 +0300

    change

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class ProdConfig:
   Args:
     Config: The parent configuration class with General configuration settings
   '''
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
   '''
--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ export MOVIE_API_KEY='559d670df4267bfc0568f5c7739dd972'
 export SECRET_KEY='123456'
 export MAIL_USERNAME='joyce.njoroge@student.moringaschool.com'
 export MAIL_PASSWORD='Wwhalien52'
-export DATABASE_URL='postgresql+psycopg2://postgres:whalien52@localhost/watchlist'
+# export DATABASE_URL='postgresql+psycopg2://postgres:whalien52@localhost/watchlist'
 
 python manage.py server
