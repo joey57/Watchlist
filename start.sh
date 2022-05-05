8e7139bd0aea387db05293ac5fc9140b589766d9
joey57 <njorogejoey5@gmail.com>
2022-05-05 17:45:21 +0300

    changed dburl

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ export MOVIE_API_KEY='559d670df4267bfc0568f5c7739dd972'
 export SECRET_KEY='123456'
 export MAIL_USERNAME='joyce.njoroge@student.moringaschool.com'
 export MAIL_PASSWORD='Wwhalien52'
-export DATABASE_URL='postgres://einbqjivkgqcsu:0e35b2a3f6fd1c83bcf0d2b07bafa84ddec9b28fbb1382336923a03791f158ae@ec2-3-228-235-79.compute-1.amazonaws.com:5432/d4ng1v9kijqorm'
+export DATABASE_URL='postgresql+psycopg2://postgres:whalien52@localhost/watchlist'
 
 python manage.py server
