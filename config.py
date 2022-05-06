d76f6ae4c4e8e0c94300d3dc603f60b14b760cc6
joey57 <njorogejoey5@gmail.com>
2022-05-06 16:21:09 +0300

    update config file

--- config.py
+++ config.py
@@ -32,7 +32,10 @@ class ProdConfig:
   Args:
     Config: The parent configuration class with General configuration settings
   '''
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  uri = os.getenv('DATABASE_URL')
+  if uri and uri.startswith('postgres://'):
+    uri = uri.replace('postgres://','postgresql://',1)
+  SQLALCHEMY_DATABASE_URI=uri
 
 class TestConfig(Config):
   '''
